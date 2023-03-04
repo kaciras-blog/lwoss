49af24ad8f19d84bc63949865700b11631c7456e
Kaciras <Kaciras@protonmail.com>
2023-03-04 18:59:43 +0800

    setup graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -84,6 +84,34 @@ async fn main() {
 
 	axum::Server::bind(&addr)
 		.serve(app.into_make_service())
+		.with_graceful_shutdown(shutdown_signal())
 		.await
 		.unwrap();
+}
+
+// https://github.com/tokio-rs/axum/blob/main/examples/graceful-shutdown
+async fn shutdown_signal() {
+	let ctrl_c = async {
+		signal::ctrl_c()
+			.await
+			.expect("failed to install Ctrl+C handler");
+	};
+
+	#[cfg(unix)]
+		let terminate = async {
+		signal::unix::signal(signal::unix::SignalKind::terminate())
+			.expect("failed to install signal handler")
+			.recv()
+			.await;
+	};
+
+	#[cfg(not(unix))]
+	let terminate = std::future::pending::<()>();
+
+	tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+	println!("Signal received, starting graceful shutdown...");
 }
