d2921b57bd83f2bec18526f226b770d52ba1d0bd
Kaciras <Kaciras@protonmail.com>
2023-03-11 19:41:09 +0800

    setup ESLint

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,20 +1,18 @@
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	extends: ["@kaciras/core", "@kaciras/typescript/base"],
+	plugins: ["svelte3"],
 	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
+		extraFileExtensions: [".svelte"],
 	},
 	env: {
 		browser: true,
-		es2017: true,
-		node: true
-	}
+	},
+	overrides: [{
+		files: ["*.svelte"],
+		processor: "svelte3/svelte3",
+	}],
+	settings: {
+		"svelte3/typescript": () => require("typescript"),
+	},
 };
