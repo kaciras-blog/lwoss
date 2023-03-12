a54908628d5eab9e64673334615675aa9a9ef247
Kaciras <Kaciras@protonmail.com>
2023-03-12 19:53:24 +0800

    miscellaneous

--- pack.py
+++ pack.py
@@ -10,4 +10,4 @@ binary = "lwoss.exe" if os == "windows" else "lwoss"
 
 with tarfile.open(f'deploy/lwoss-{os}.tar.xz', 'w:xz') as tar:
     tar.add("web/build", "web")
-    tar.add(f"target/debug/{binary}", binary)
+    tar.add(f"target/release/{binary}", binary)
