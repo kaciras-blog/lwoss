3d924ec9e13a67a28ad90d22b361697e9ea4afdc
Kaciras <Kaciras@protonmail.com>
2023-03-12 19:43:29 +0800

    pack assets using Python script

---
+++ pack.py
@@ -0,0 +1,13 @@
+import platform
+import tarfile
+from os import mkdir
+
+os = platform.system().lower()
+
+mkdir("deploy")
+
+binary = "lwoss.exe" if os == "windows" else "lwoss"
+
+with tarfile.open(f'deploy/lwoss-{os}.tar.xz', 'w:xz') as tar:
+    tar.add("web/build", "web")
+    tar.add(f"target/debug/{binary}", binary)
