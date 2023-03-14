cdb58944018310a9d8504fa7c02e781589061ae4
Kaciras <Kaciras@protonmail.com>
2023-03-14 21:09:04 +0800

    add tests for range.rs

--- src/range.rs
+++ src/range.rs
@@ -1,10 +1,9 @@
 use std::fs::Metadata;
-use std::io::{ErrorKind, Seek, SeekFrom};
+use std::io::{ErrorKind, SeekFrom};
 use std::ops::RangeInclusive;
-use std::path::PathBuf;
+use std::path::Path;
 
-use axum::body::{Bytes, Empty, HttpBody, StreamBody};
-use axum::Error;
+use axum::body::{Empty, HttpBody, StreamBody};
 use axum::http::{HeaderMap, StatusCode};
 use axum::http::response::Builder;
 use axum::response::{IntoResponse, Response};
@@ -12,15 +11,14 @@ use futures::{FutureExt, StreamExt};
 use http_range_header::parse_range_header;
 use httpdate::fmt_http_date;
 use tokio::fs::File;
-use tokio::io::AsyncSeekExt;
+use tokio::io::{AsyncReadExt, AsyncSeekExt};
 use tokio_util::io::ReaderStream;
-use tower_http::limit::ResponseBody;
 
-pub async fn send_file_range(headers: HeaderMap, file: PathBuf, mime: String) -> Response {
-	match File::open(file).await {
+pub async fn send_file(headers: HeaderMap, path: impl AsRef<Path>, mime: &str) -> Response {
+	match File::open(path).await {
 		Ok(file) => {
 			match file.metadata().await {
-				Ok(attrs) => send_range(headers, file, attrs, mime).await,
+				Ok(attrs) => send(headers, file, attrs, mime).await,
 				Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response()
 			}
 		}
@@ -34,7 +32,7 @@ pub async fn send_file_range(headers: HeaderMap, file: PathBuf, mime: String) ->
 }
 
 /// https://github.com/tower-rs/tower-http/blob/master/tower-http/src/services/fs/serve_dir/future.rss
-async fn send_range(headers: HeaderMap, file: File, attrs: Metadata, mime: String) -> Response {
+async fn send(headers: HeaderMap, file: File, attrs: Metadata, mime: &str) -> Response {
 	let builder = Response::builder()
 		.header("Accept-Ranges", "bytes")
 		.header("Last-Modified", fmt_http_date(attrs.modified().unwrap()));
@@ -48,7 +46,8 @@ async fn send_range(headers: HeaderMap, file: File, attrs: Metadata, mime: Strin
 
 		if let Some(ranges) = ranges {
 			if ranges.len() == 1 {
-				return single(builder, file, ranges[0].clone(), mime).await;
+				let range = ranges[0].to_owned();
+				return single(builder, file, attrs, range, mime).await;
 			}
 			log::warn!("Can not handle request with multiple ranges.")
 		}
@@ -68,16 +67,101 @@ async fn send_range(headers: HeaderMap, file: File, attrs: Metadata, mime: Strin
 	}
 }
 
-async fn single(builder: Builder, mut file: File, x: RangeInclusive<u64>, mime: String) -> Response {
+async fn single(builder: Builder, mut file: File, attrs: Metadata, x: RangeInclusive<u64>, mime: &str) -> Response {
 	let size = x.end() - x.start() + 1;
 
 	// There is only one seek in progress, so it don't return Err.
 	file.seek(SeekFrom::Start(*x.start())).await.unwrap();
-	let reader = ReaderStream::new(file);
+	let reader = ReaderStream::new(file.take(size));
 
 	builder.status(StatusCode::PARTIAL_CONTENT)
 		.header("Content-Length", size)
 		.header("Content-Type", mime)
-		.header("Content-Range", format!("bytes {}-{}/{}", x.start(), x.end(), size))
-		.body(StreamBody::new(reader.take(size as usize)).boxed_unsync()).unwrap()
+		.header("Content-Range", format!("bytes {}-{}/{}", x.start(), x.end(), attrs.len()))
+		.body(StreamBody::new(reader).boxed_unsync()).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+	use axum::body::{BoxBody, HttpBody};
+	use axum::http::HeaderMap;
+	use hyper::body::to_bytes;
+
+	use crate::range::send_file;
+
+	const FILE: &str = "test-files/sendrange.txt";
+
+	async fn assert_body(actual: BoxBody, expected: &[u8]) {
+		assert_eq!(to_bytes(actual).await.unwrap().as_ref(), expected);
+	}
+
+	#[tokio::test]
+	async fn not_found() {
+		let (p, b) = send_file(HeaderMap::new(), "404", "text/plain").await.into_parts();
+		insta::assert_debug_snapshot!(p);
+		assert_eq!(b.is_end_stream(), true);
+	}
+
+	#[tokio::test]
+	async fn invalid_range() {
+		let mut headers = HeaderMap::new();
+		headers.append("Range", "foobar".try_into().unwrap());
+
+		let (p, b) = send_file(headers, FILE, "text/plain").await.into_parts();
+
+		insta::assert_debug_snapshot!(p);
+		assert_eq!(b.is_end_stream(), true);
+	}
+
+	#[tokio::test]
+	async fn non_range() {
+		let (p, b) = send_file(HeaderMap::new(), FILE, "text/plain").await.into_parts();
+
+		insta::assert_debug_snapshot!(p);
+		assert_body(b, std::fs::read(FILE).unwrap().as_slice()).await;
+	}
+
+	#[tokio::test]
+	async fn single() {
+		let mut headers = HeaderMap::new();
+		headers.append("Range", "bytes=1-3".try_into().unwrap());
+
+		let (p, b) = send_file(headers, FILE, "text/html").await.into_parts();
+
+		insta::assert_debug_snapshot!(p);
+		assert_body(b, b"f m").await;
+	}
+
+	#[tokio::test]
+	async fn begin_only() {
+		let mut headers = HeaderMap::new();
+		headers.append("Range", "bytes=470-".try_into().unwrap());
+
+		let (p, b) = send_file(headers, FILE, "text/html").await.into_parts();
+
+		insta::assert_debug_snapshot!(p);
+		assert_body(b, b"ead).").await;
+	}
+
+	#[tokio::test]
+	async fn end_only() {
+		let mut headers = HeaderMap::new();
+		headers.append("Range", "bytes=-2".try_into().unwrap());
+
+		let (p, b) = send_file(headers, FILE, "text/html").await.into_parts();
+
+		insta::assert_debug_snapshot!(p);
+		assert_body(b, b").").await;
+	}
+
+	#[tokio::test]
+	async fn multiple() {
+		let mut headers = HeaderMap::new();
+		headers.append("Range", "bytes=80-83,429-472,294-304".try_into().unwrap());
+
+		let (p, b) = send_file(headers, FILE, "text/html").await.into_parts();
+
+		insta::assert_debug_snapshot!(p);
+		assert_eq!(b.is_end_stream(), true);
+	}
 }
